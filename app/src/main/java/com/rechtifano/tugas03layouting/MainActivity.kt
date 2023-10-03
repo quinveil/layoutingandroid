e55c42cddf84d97fa5301b5ea7c4f8faed6c7abf
quinveil <quinveil@quinveil.com>
2023-10-03 11:42:35 +0700

    initial

---
+++ app/src/main/java/com/rechtifano/tugas03layouting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rechtifano.tugas03layouting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
